6b39a00bf9f8c4cd2f6d40529905f020cb5d69fc
iamfranco <kinfaifrancochan@gmail.com>
2023-10-29 17:24:10 +0000

    Add omdbService to get movie details from OMdb
    and a placeholder MovieButton to console log the response

---
+++ src/components/MovieButton.tsx
@@ -0,0 +1,15 @@
+import { omdbService } from '../services/omdbService'
+
+const MovieButton = () => {
+
+  const handleClick = async () => {
+    const movie = await omdbService.getMovie('Guardians of the Galaxy Vol. 3');
+    console.log(movie);
+  }
+
+  return (
+    <button onClick={handleClick}>MovieButton</button>
+  )
+}
+
+export default MovieButton
